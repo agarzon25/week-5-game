4f5c71db3de100cf2a042d2ba5adcfe48a88cc44
Andrew Garzon <andrew.garzon.25@gmail.com>
2018-04-17 20:46:48 -0400

    committing latest working copy

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 	// Creating a variable for amount of seconds for the timer
-	var seconds = 31;
+	var seconds = 30;
 
 	// Setting a variable for the question current question number
 	var qn = 1;
@@ -14,28 +14,23 @@ $(document).ready(function() {
 
 	// questions object
 	var questions = {
-		q1 : {question: "question1", answer: "answer 1"},
-		q2 : {question: "question2", answer: "answer 2"},
-		q3 : {question: "question3", answer: "answer 3"}
- 	};
-
- 	// options object
-	var options = {
-		o1 : [questions.q1.answer, "guess2", "guess3", "guess4"],
-		o2 : [questions.q2.answer, "guess2", "guess3", "guess4"],
-		o3 : [questions.q3.answer, "guess2", "guess3", "guess4"],
-	};	
+		q1 : {question: "question1", answer: "answer 1", options: ["answer 1", "guess2", "guess3", "guess4"]},
+		q2 : {question: "question2", answer: "answer 2", options: ["answer 2", "guess2", "guess3", "guess4"]},
+		q3 : {question: "question3", answer: "answer 3", options: ["answer 3", "guess2", "guess3", "guess4"]}
+ 	};	
 
 	// Creating timer to give user 30 second to answer the question
 	function timer() {
 		seconds--;
 		if (seconds<0) {
-			clearInterval(tick);
+			clearInterval();
+			return
 		} else if (seconds === 0 && qn < Object.keys(questions).length) {
 			seconds = 30;
 			qn++;
-			currentQuestion(qn)
-		} 
+			currentQuestion(qn);
+			return
+		}
 		// print out time left on the clock
 		$("#time").html(seconds)
 	}
@@ -44,42 +39,44 @@ $(document).ready(function() {
 	function currentQuestion(n) {
 
 		// Display the current question
-		$("#question").text(questions["q" + n].question)
+		$("#question").html(questions["q" + n].question)
 
 		// Creating an empty array to hold all option elements
 		var opts = [];
 
+		// options variable
+
+		options = questions["q" + n].options
+
 		// Creating a button div for each option
-		options["o" + n].forEach(b => {
+		options.forEach( b => {
 			
 			console.log(b)
-			btnType = $("<button class='btn btn-outline-danger btn-block' id='choice' value='"+ b +"'>")
-			const btn = btnType.html(b)
-			//btnType.attr("value", b)
-			opts.push(btn)
-
+			btnType = $("<button data-name='" + b + "' class='btn btn-outline-danger btn-block answerButton'>" + b + "</button>")
+			opts.push(btnType)
 		})
 
 		// Append opts to answers div
 		$("#answers").html(opts)
 	
-		guess()
 	}
 
 	function guess() {
-		$("#answers").click(function() {
-			$('#choice').each(function() {
-				console.log($("#choice").attr("value"))
+		$("#answers").on("click", () => {
+			// e.stopPropagation();
+			$(".answerButton").each(function() {
+				// console.log($(this))
+				console.log($(this).attr("data-name"))
 			})
 		})
 	}
 
 	// Hide time, question, answers div when document loads
+
 	function hideDivs() {
 		$("#time").hide();
 		$("#question").hide();
 		$("#answers").hide();
-
 	}
 
 	// show time, question and answers divs
@@ -87,8 +84,6 @@ $(document).ready(function() {
 		$("#time").show();
 		$("#question").show();
 		$("#answers").show();
-
-		// Hide start button
 		$("#button").hide();
 	}
 
@@ -99,10 +94,18 @@ $(document).ready(function() {
 	// click start to begin the game
 	$("#button").click(function() {
 		// Creating an element for interval variable
-		var tick = setInterval(timer, 1000);
+		setInterval(timer, 1000);
 		showDivs();
-		currentQuestion(qn);
-		timer();
-		//console.log(guess)
+		if (qn > Object.keys(questions).length) {
+			console.log("made it")
+			hideDivs()
+			$("#answers").html("Game Over!")
+			return
+		} else {
+			currentQuestion(qn);
+			guess();	
+		}
+		//timer();
+		return
 	})
 })
